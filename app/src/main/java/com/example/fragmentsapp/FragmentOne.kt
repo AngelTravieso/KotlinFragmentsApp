d594d317e5949bc3ac93eef67575482ef88cf9ce
AngelTravieso <angeltraviesoc@gmail.com>
2024-02-03 11:54:36 -0400

    Fragments LifeCycle App

--- app/src/main/java/com/example/fragmentsapp/FragmentOne.kt
+++ app/src/main/java/com/example/fragmentsapp/FragmentOne.kt
@@ -1,20 +1,74 @@
 package com.example.fragmentsapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 
 class FragmentOne : Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Toast.makeText(context, "onAttach() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(context, "onCreate() is Called", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        Toast.makeText(context, "onCreateView() is Called", Toast.LENGTH_SHORT).show()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }*/
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(context, "onStart() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(context, "onResume() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(context, "onPause() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(context, "onStop() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Toast.makeText(context, "onDestroyView() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(context, "onDestroy() is Called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Toast.makeText(context, "onDetach() is Called", Toast.LENGTH_SHORT).show()
     }
 
 }
